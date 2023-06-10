05238a577513c5091a6726e43c261f666837e301
Bhargav-j <jeereddybhargav@gmail.com>
2023-06-10 17:58:56 +0530

    empty_branch

--- Clone Projects/Youtube-clone/main.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const sideBar = document.querySelector('.sidebar');
-const Contianer = document.querySelector('.container');
-
-
-menuIcon.onclick = function(){
-    sideBar.classList.toggle("small-sidebar");
-    Contianer.classList.toggle("large-container");
-}
